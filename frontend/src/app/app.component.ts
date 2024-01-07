14ea1e89facf897442f8e6d0427a2be13de45f19
leliw <49598388+leliw@users.noreply.github.com>
2024-01-07 13:35:16 +0100

    Run in development environment

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
+export interface Hello {
+    Hello: string;
+}
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet, HttpClientModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'frontend';
+
+    title = 'frontend';
+    hello = '';
+
+    constructor(private http: HttpClient) {
+        this.http.get<Hello>('/api').subscribe(data => {
+            this.hello = data.Hello;
+        });
+    }
+
 }
