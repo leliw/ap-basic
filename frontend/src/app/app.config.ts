d078821f8bcbcaf1c305d0264aaf2b4cd330ba28
Marcin Leliwa <marcin.leliwa@gmail.com>
2024-02-13 12:19:34 +0100

    Upgrade Angular to 17.1.3

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,10 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import {provideHttpClient} from '@angular/common/http';
-
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideHttpClient()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient()]
 };
